f093982b2125caf59c90d53628355d3248633d57
Rick Chadwick <rick@madeinbeta.co.uk>
2016-02-17 10:17:07 +0000

    in the middle of the slides but been disturbed

--- _dev/scripts/slides.js
+++ _dev/scripts/slides.js
@@ -1,18 +1,3 @@
-/**
- * acollective.co
- * Copyright 2016 Rick Chadwick
- * @author rickary <rick@rickchadwick.co.uk>
- */
-
-$('.js-hero').css('height', $(window).height());
-
-
-$(document).ready(function(){
-    var length = $(".js-hero-captions li").length;
-	var ran = Math.floor(Math.random()*length) + 1;
-	$(".js-hero-captions li:nth-child(" + ran + ")").css('display', 'table-cell');
-});
-
 
 $(".js-slides").responsiveSlides({
   auto: true,             // Boolean: Animate automatically, true or false
--- perch/templates/layouts/multi-slides.php
+++ perch/templates/layouts/multi-slides.php
@@ -1,5 +1,32 @@
 <section class="p-section <?php echo perch_layout_var('colour'); ?>">
 	<div class="b-container">
 		<h2 class="p-section__title">The Space</h2>
+
+
+
+		<div class="p-slides p-slides--large">
+			<?php perch_content('Large Slides') ?>
+		</div>
+
+		<div class="p-two-slides">
+			<div class="p-two-slides__inner">
+				<ul class="slides js-slides p-slides--small">
+					<li class="slide"><img src="http://placehold.it/500x500&text=one" alt="one" class="slide__img" /></li>
+					<li class="slide"><img src="http://placehold.it/500x500&text=two" alt="two" class="slide__img" /></li>
+					<li class="slide"><img src="http://placehold.it/500x500&text=three" alt="three" class="slide__img" /></li>
+					<li class="slide"><img src="http://placehold.it/500x500&text=four" alt="four" class="slide__img" /></li>
+				</ul>
+			</div>
+
+			<div class="p-two-slides__inner">
+				<ul class="slides js-slides p-slides--medium">
+					<li class="slide"><img src="http://placehold.it/580x720&text=one" alt="one" class="slide__img" /></li>
+					<li class="slide"><img src="http://placehold.it/580x720&text=two" alt="two" class="slide__img" /></li>
+					<li class="slide"><img src="http://placehold.it/580x720&text=three" alt="three" class="slide__img" /></li>
+					<li class="slide"><img src="http://placehold.it/580x720&text=four" alt="four" class="slide__img" /></li>
+				</ul>
+			</div>
+		</div>
+
 	</div>
 </section>
